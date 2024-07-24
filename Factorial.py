752078cc612ca090e8535fe8dfefc99fcfc84541
Kauet-Martins <kauetdamotta@gmail.com>
2024-07-24 20:59:44 -0300

    First Exercise

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+
+def Factorial(num):
+    
+    if num == 0:
+        return 1
+    
+    return num * Factorial(num - 1)
+
+print(Factorial(4))
---
+++ Palindromo.py
@@ -0,0 +1,5 @@
+
+def Palindromo(a):
+    return a == a[::-1]
+    
+print(Palindromo("apos a sopa"))
